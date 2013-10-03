7d2d6f2122fda2798796e00de6793f6f0b651b5f
erp12 <erp12@hampshire.edu>
2013-10-02 22:03:54 -0400

    Added Interpreter, does nothing

---
+++ pyshInterpreter.py
@@ -0,0 +1,16 @@
+'''
+Created on Oct 2, 2013
+
+@author: Eddie
+'''
+
+class PyshInterpreter(object):
+    '''
+    classdocs
+    '''
+    stacks = {}
+
+    def __init__(self):
+        '''
+        Constructor
+        '''
