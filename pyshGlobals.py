0b9a994867c2b6d615ab8b129906759c40ef0489
erp12 <erp12@hampshire.edu>
2013-10-05 07:15:51 -0400

    Added way to get random instructions or numbers
    Getting random instructions involves getting random line from
    instruction list text file.

--- pyshGlobals.py
+++ pyshGlobals.py
@@ -3,16 +3,22 @@ Created on Oct 2, 2013
 
 @author: Eddie
 '''
+import random
 
 class Exec(object):
     symbol = None
     def __init__(self, sym):
         self.symbol = sym
 
-def random(typ, min = None, max = None):
-    if typ is int:
-        return random(min, max)
-    elif type is Exec:
+def rand(typ, min = None, max = None):
+    if typ == int:
+        return random.randint(min, max)
+    elif typ is Exec:
+        print('Hit')
+        line = random.choice(open('PyshInstructionsList.txt').readlines())
+        if line[:1] == '#':
+            return rand(Exec)
+        else:
+            return line
         
-
 pysh_types = [int, float, Exec]
