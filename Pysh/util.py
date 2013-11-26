537775b6b8ef6ae423185a28bf4f91635928febc
erp12 <erp12@hampshire.edu>
2013-11-26 14:31:46 -0500

    started push to python

--- Pysh/util.py
+++ Pysh/util.py
@@ -4,7 +4,27 @@ Created on Oct 30, 2013
 @author: Eddie Pantridge Hampshire College
 '''
 import globals
-
+        
+def push_to_python(pushCode):
+    pyString = []
+    for c in pushCode:
+        if c == '(':
+            pyString.append('[')
+        elif c == ')':
+            pyString.append(']')
+        elif c == ' ':
+            pyString.append(', ')
+        else:
+            pyString.append(c)
+    pyString = ''.join(pyString)
+    print pyString
+    
+    #Add ' ' around strings and it should work
+    
+    pyList = eval(pyString)
+    return pyList
+    
+print push_to_python('(1 (3 2 int_mult) (5 2 int_add) int_mult)')
 
 def keep_number_reasonable(n):
     '''
